747894fd82540b94fca5ae656a276ed662588f68
Navaneeth_Krishna <95477780+navaneethkrishna11@users.noreply.github.com>
2025-06-03 15:05:19 +0530

    Create Count of n input.c

---
+++ Small programs/Count of n input.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <string.h>
+int main() {
+    int i,j,n;
+    printf("enter the numer limit : ");
+    scanf("%d",&n);
+    char str[32];
+    sprintf(str,"%d",n);
+    printf("String: %d\n", strlen(str));
+  
+}
+
+/*
+Output
+enter the numer limit : 43242
+String: 5
+*/
