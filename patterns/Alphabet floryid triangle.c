4c078bd7ccdbd5749188250b1d770bff6246dc7a
Navaneeth_Krishna <95477780+navaneethkrishna11@users.noreply.github.com>
2025-06-08 11:27:21 +0530

    Update Alphabet floryid triangle.c

--- patterns/Alphabet floryid triangle.c
+++ patterns/Alphabet floryid triangle.c
@@ -52,3 +52,72 @@ ABC
 AB
 A
 */
+
+#include<stdio.h>
+#include<string.h>
+
+void main(){
+    int n=10;
+    char ch;
+    for (int i=0;i<n;i++)
+    {
+        ch= 'A'+i;
+        for(int j=0;j<=i;j++)
+        {
+            printf("%c",ch);
+        }
+        printf("\n");
+    }
+}
+
+/*
+A
+BB
+CCC
+DDDD
+EEEEE
+FFFFFF
+GGGGGGG
+HHHHHHHH
+IIIIIIIII
+JJJJJJJJJJ
+
+*/
+
+#include<stdio.h>
+#include<string.h>
+
+void main(){
+    int n=10;
+    char ch;
+    for (int i=0;i<n;i++)
+    {
+      
+        for(int j=0;j<n-i;j++)
+        { 
+            printf(" ");
+        }
+        
+        for(int j=0;j<i;j++)
+        {
+            ch = 'A'+ j;
+            printf("%c ",ch);
+        }
+        printf("\n");
+    }
+}
+
+/*
+         A 
+        A B 
+       A B C 
+      A B C D 
+     A B C D E 
+    A B C D E F 
+   A B C D E F G 
+  A B C D E F G H 
+ A B C D E F G H I 
+
+
+*/
+
